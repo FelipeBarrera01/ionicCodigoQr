8ee331645ba21e817e414f807d0f48f46ed32913
Juan Felipe Barrera <juan.F.barera01@gmail.com>
2019-09-11 10:40:54 -0500

    aplicación terminada

--- src/app/pages/mapa/mapa.page.ts
+++ src/app/pages/mapa/mapa.page.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+declare var mapboxgl: any;
 @Component({
   selector: 'app-mapa',
   templateUrl: './mapa.page.html',
   styleUrls: ['./mapa.page.scss'],
 })
-export class MapaPage implements OnInit {
+export class MapaPage implements OnInit, AfterViewInit{
 lat: number;
 lng: number;
   constructor(private router: ActivatedRoute) { }
@@ -15,8 +15,64 @@ lng: number;
     let geo : any = this.router.snapshot.paramMap.get('geo');
     geo = geo.substr(4);
     geo = geo.split(',');
-    this.lat = geo[0];
-    this.lng = geo[1];
+    this.lat = Number( geo[0]);
+    this.lng = Number( geo[1]);
   }
+ngAfterViewInit(){
+  mapboxgl.accessToken = 'pk.eyJ1IjoiZmVsaXBlYmFycmVyYTk5IiwiYSI6ImNrMGVlZzkzazBoY3ozbHFkMm9hcmVpZ3MifQ.7oPfV80jYdn7lEIpP362eg';
+  const map = new mapboxgl.Map({
+    style: 'mapbox://styles/mapbox/light-v10',
+    center: [-74.0066, 40.7135],
+    zoom: 15.5,
+    pitch: 45,
+    bearing: -17.6,
+    container: 'map',
+    antialias: true
+    });
+    map.on('load', () => {
+      map.resize();
 
+       new mapboxgl.Marker({
+        draggable: true
+        })
+        .setLngLat([0, 0])
+        .addTo(map);
+      // Insert the layer beneath any symbol layer.
+      const layers = map.getStyle().layers;
+       
+      let labelLayerId;
+      for (var i = 0; i < layers.length; i++) {
+      if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+      labelLayerId = layers[i].id;
+      break;
+      }
+      }
+       
+      map.addLayer({
+      'id': '3d-buildings',
+      'source': 'composite',
+      'source-layer': 'building',
+      'filter': ['==', 'extrude', 'true'],
+      'type': 'fill-extrusion',
+      'minzoom': 15,
+      'paint': {
+      'fill-extrusion-color': '#aaa',
+       
+      // use an 'interpolate' expression to add a smooth transition effect to the
+      // buildings as the user zooms in
+      'fill-extrusion-height': [
+      "interpolate", ["linear"], ["zoom"],
+      15, 0,
+      15.05, ["get", "height"]
+      ],
+      'fill-extrusion-base': [
+      "interpolate", ["linear"], ["zoom"],
+      15, 0,
+      15.05, ["get", "min_height"]
+      ],
+      'fill-extrusion-opacity': .6
+      }
+      }, labelLayerId);
+      });
+}
 }
--- src/app/services/data-local.service.ts
+++ src/app/services/data-local.service.ts
@@ -3,14 +3,27 @@ import { registro } from '../models/registro.model';
 import { Storage } from '@ionic/storage';
 import { NavController } from '@ionic/angular';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { File } from '@ionic-native/file/ngx';
+import { EmailComposer } from '@ionic-native/email-composer/ngx';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataLocalService {
 guardados: registro[] = [];
-constructor(private storage: Storage, private navCtrl: NavController,private iab: InAppBrowser){
+constructor(private storage: Storage, private navCtrl: NavController
+  ,private iab: InAppBrowser, private file: File,private emailComposer: EmailComposer){
   this.cargarStora();
+ }
+ enviarCorreo(){
+   const arrTemp = [];
+   const titulos = 'Tipo, Formato, Creado en, Texto\n';
+   arrTemp.push(titulos);
+   this.guardados.forEach(registro =>{
+     const linea = `${registro.type}, ${registro.format}, ${registro.created}, ${registro.text.replace(',', ' ')}\n`;
+    arrTemp.push(linea);
+    });
+    this.crearArchivoFisico(arrTemp.join(''));
  }
 async cargarStora(){
   this.guardados = await this.storage.get('registros') || [];
@@ -34,5 +47,37 @@ async cargarStora(){
           this.navCtrl.navigateForward(`tabs/tabs2/mapa/${regis.text}`);
           break;
     }
+  }
+  crearArchivoFisico( text: string ){
+    this.file.checkFile(this.file.dataDirectory, 'registros.csv')
+    .then( existe => {
+      return this.escribirEnArchivo(text);
+    }).catch(err =>{
+      return this.file.createFile(this.file.dataDirectory, 'registros.csv', false)
+      .then( creado =>{
+        this.escribirEnArchivo(text);
+      }).catch(er =>{
+        console.log(er);
+
+      });
+    });
+  }
+  async escribirEnArchivo(text: string){
+   await this.file.writeExistingFile(this.file.dataDirectory, 'registros.csv', text);
+   const archivo = `${this.file.dataDirectory}/registros.csv`
+   const email = {
+    to: 'juan.f.barrera@gmail.com',
+  
+   
+    attachments: [
+      archivo
+    ],
+    subject: 'Backup de scans',
+    body: 'How are you? Nice greetings from Leipzig',
+    isHtml: true
+  }
+  
+  // Send a text message using default options
+  this.emailComposer.open(email);
   }
 }
